ae2df126acee34b160c842393ab3c6c8135be938
yo16 <xxyo1xx@gmail.com>
2024-03-24 16:07:07 +0900

    svg外の要素

--- canvas_prac/src/MySvg1.js
+++ canvas_prac/src/MySvg1.js
@@ -5,13 +5,17 @@ import "./MySvg1.css";
 const MySvg1 = () => {
     return (
         <div>
-            sample1. TransformComponentの中にsvgタグ
-            <TransformWrapper>
+            react-zoom-pan-pinchを使った例
+            <TransformWrapper
+                minScale={0.1}
+            >
                 <TransformComponent>
-                    <svg width={400} height={400} >
+                    <svg width={550} height={400} style={{backgroundColor: "#dde"}} >
                         <rect x="50" y="50" width="100" height="100" fill="blue" />
                         <circle cx="250" cy="150" r="50" fill="red" />
                         <text x="50" y="250" fontSize="20" fill="green">Hello, SVG!</text>
+                        <rect x="350" y="50" width="200" height="100" fill="lightblue" />
+                        <circle cx="460" cy="150" r="50" fill="green" />
                     </svg>
                 </TransformComponent>
             </TransformWrapper>
